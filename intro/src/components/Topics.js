f59a133c6fae28180898c11d9aa33298092409a3
Gabriel Dyck <gabriel.dyck@despegar.com>
2019-05-25 20:56:41 -0300

    use match react param for more dynamic and flex router

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -6,7 +6,7 @@ Route} from 'react-router-dom'
 function Topic({match}){
 	return (<h3>{match.params.topicId}</h3>)
 }
-export default function Topics () {
+export default function Topics ({match}) {
 	return (
 
 		<div>
@@ -14,14 +14,14 @@ export default function Topics () {
 
 		<ul>
 			<li>
-				<Link to="/topics/rendering">Rendering with React</Link>
+				<Link to={`${match.url}/rendering`}>Rendering with React</Link>
 			</li>
 
 			<li>
-				<Link to="/topics/components">Components</Link>
+				<Link to={`${match.url}/components}`}>Components</Link>
 			</li>
 			<li>
-				<Link to="/topics/props-v-state">Props vs State</Link>
+				<Link to={`${match.url}/props-v-state`}>Props vs State</Link>
 			</li>
 		</ul>
 
