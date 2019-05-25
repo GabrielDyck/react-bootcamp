b77f733d66ee709bb913f12bbbcdf7a0b8b9b8fc
Gabriel Dyck <gabriel.dyck@despegar.com>
2019-05-25 20:31:01 -0300

    routing. add exact for full path in / because matches full qualify path

--- intro/src/components/App.js
+++ intro/src/components/App.js
@@ -14,12 +14,18 @@ class App extends Component {
   render (){
     return (
     <BrowserRouter>
+    <div>
     <ul>
     //Be have to wrap this links in BrowserRouter
     <li><Link to="/"> Home </Link></li>
     <li><Link to="/about"> About </Link></li>
     <li><Link to="/topics"> Topics </Link></li>
     </ul>
+    <hr/>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/topics" component={Topics} />
+    </div>
     </BrowserRouter>
         )
   }
